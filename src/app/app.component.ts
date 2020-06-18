ba07e9596e94f7e82b95563073ea6225d0db0bdf
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-06-18 01:34:11 -0300

    chore: upload project to repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  phrase: string = lorem.sentence();
+  enteredText: string = '';
+
+  onInput(value: string): void {
+    this.enteredText = value;
+  }
+
+  getClass(letter: string, enteredLetter: string): string {
+    if (!enteredLetter) return;
+
+    return letter === enteredLetter
+      ? 'has-text-success has-text-weight-bold'
+      : 'has-text-danger has-text-weight-bold';
+  }
 }
